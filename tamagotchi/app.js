cb968e19f94dd408e494741e05b462fdba50e70d
Sorin <zece.delfini@outlook.com>
2023-02-26 18:55:36 +0200

    c1

--- tamagotchi/app.js
+++ tamagotchi/app.js
@@ -1,12 +1,37 @@
+const hungerStat = document.querySelector(".hunger-stat");
+const boredomStat = document.querySelector(".boredom-stat");
+const neatnessStat = document.querySelector(".neatness-stat");
+const sizeStat = document.querySelector(".size-stat");
+
+const content = document.querySelector(".content");
+const message = document.querySelector(".message");
+const feedBtn = document.querySelector(".feed-btn");
+const playBtn = document.querySelector(".play-btn");
+const cleanBtn = document.querySelector(".clean-btn");
+
 const img = new Image();
 img.src = "./img/dog.png";
+
+const imgWidth = img.width;
+const imgHeight = img.height;
+
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width=300;
+canvas.height=200;
+ctx.fillStyle = "#eee";
+ctx.fillRect(50, 0, canvas.width, canvas.height);
+content.appendChild(canvas);
 
 class Dog {
     constructor(){
         this.hunger = 0;
         this.boredom = 100;
-        this.neatness = 0;
+        this.neatness = 100;
         this.size = 30;
+        this.boredFlag = true;
+        this.hungerFlag = true;
+        this.dirtyFlag = true;
     }
     feed(){
         if(this.hunger > 0){
@@ -19,8 +44,8 @@ class Dog {
         }
     }
     clean(){
-        if(this.neatness > 0){
-            this.neatness -= 50;
+        if(this.neatness < 100){
+            this.neatness += 50;
         }
     }
     metabolyze(){
@@ -30,14 +55,30 @@ class Dog {
         if(this.boredom < 100){
             this.boredom++;
         }
-        if(this.neatness < 100){
-            this.neatness++;
+        if(this.neatness > 1){
+            this.neatness--;
         }
         if(this.hunger < 20){
             if(this.size < 126){
                 this.size++;
             }
         }
+        if(this.boredom > 99){
+            this.boredFlag = true;
+        } else {
+            this.boredFlag = false;
+        }
+        if(this.hunger > 30){
+            this.hungryFlag = true;
+        } else{
+            this.hungryFlag = false;
+        }
+        if(this.neatness > 30){
+            this.dirtyFlag = true;
+        } else{
+            this.dirtyFlag = false;
+        }
+
     }
     updateCanvas(){
         ctx.fillStyle = "#eee";
@@ -51,6 +92,18 @@ class Dog {
         neatnessStat.innerHTML = `Neatness: ${this.neatness}`;
         sizeStat.innerHTML = `Size: ${this.size}`;
     }
+
+    displayMessage() {
+        if(this.boredFlag && !message.innerHTML.includes("bored")){
+            message.innerHTML += "I am bored! ";
+        } else {
+            message.innerHTML = "";
+        }
+        if(this.hungryFlag && !message.innerHTML.includes("hungry")){
+            message.innerHTML += "I am hungry! ";
+        }
+
+    }
 }
 
 const dog = new Dog();
@@ -58,7 +111,7 @@ const dog = new Dog();
  //incarcare imagine
  img.addEventListener("load", loadImage, false);
  function loadImage(e) {
-    console.log(e);
+    //console.log(e);
     ctx.drawImage(img, 50, 0, imgWidth * dog.size / 100, imgHeight * dog.size / 100);
  }
 
@@ -67,30 +120,8 @@ setInterval(()=>{
     dog.metabolyze();
     dog.updateCanvas();
     dog.updateStats();
+    dog.displayMessage();
 },1000);
-
-
-const hungerStat = document.querySelector(".hunger-stat");
-const boredomStat = document.querySelector(".boredom-stat");
-const neatnessStat = document.querySelector(".neatness-stat");
-const sizeStat = document.querySelector(".size-stat");
-
-const content = document.querySelector(".content");
-const feedBtn = document.querySelector(".feed-btn");
-const playBtn = document.querySelector(".play-btn");
-const cleanBtn = document.querySelector(".clean-btn");
-
-const imgWidth = img.width;
-const imgHeight = img.height;
-
-const canvas = document.createElement('canvas');
-const ctx = canvas.getContext('2d');
-canvas.width=300;
-canvas.height=200;
-ctx.fillStyle = "#eee";
-ctx.fillRect(60, 0, canvas.width, canvas.height);
-content.appendChild(canvas);
-
 
 
 feedBtn.addEventListener("click", ()=>{
