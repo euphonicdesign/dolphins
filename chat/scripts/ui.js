a7e4f6f1ed9e8cdd4ec2b295cdbf23ff1122d1c3
Sorin <zece.delfini@outlook.com>
2023-02-26 17:39:30 +0200

    c3

--- chat/scripts/ui.js
+++ chat/scripts/ui.js
@@ -9,15 +9,18 @@ class ChatUI {
         this.list.innerHTML = "";
     }
     render(data) {
-        const when = dateFns.distanceInWordsToNow(
-            data.created_at.toDate(),
-            { addSuffix:true }
-        )
+        // const when = dateFns.distanceInWordsToNow(
+        //     data.created_at.toDate(),
+        //     { addSuffix:true }
+        // )
+        const when = data.created_at.toDate();
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: "numeric", minute:"numeric" };
+
         const html = `
             <li class="list-group-item">
                 <span class="username">${data.username}</span>
                 <span class="message">${data.message}</span>
-                <div class="time">${when}</div>
+                <div class="time">${when.toLocaleDateString("en-US", options)}</div>
             </li>
         `;
 
