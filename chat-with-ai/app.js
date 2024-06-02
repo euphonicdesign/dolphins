fe913ebf06237b354a9463b06396429f9ec9ec9c
Sorin <zece.delfini@outlook.com>
2024-06-02 16:16:01 +0300

    added chat-with-ai app

---
+++ chat-with-ai/app.js
@@ -0,0 +1,36 @@
+const formElement = document.querySelector("form");
+// console.log(formElement);
+
+const botResponsesUlElement = document.querySelector(".bot-responses");
+
+formElement.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = formElement.elements.msg.value;
+  console.log("Form submitted: " + formElement.elements.msg.value);
+
+  const url = "http://localhost:3000/chatwithai";
+
+  let data = {
+    msg: message,
+  };
+
+  let request = new Request(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: new Headers({
+      "Content-Type": "application/json; charset=UTF-8",
+    }),
+  });
+
+  fetch(request)
+    .then((response) => {
+      // Handle response you get from the API
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const botResponseLiElement = document.createElement("li");
+      botResponseLiElement.innerText = data;
+      botResponsesUlElement.appendChild(botResponseLiElement);
+    });
+});
